1c4ef138e9fba2ab45bcbce0126ff239b14f0868
Md Mohsin Siddiqui <mdmohsinsiddiqui5@gmail.com>
2023-02-04 14:33:57 +0530

    updated  main.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::{serde::{json::{json, Value}}};
+
+// add routes and services modules here
+mod routes;
+mod services;
+
+// import routes here
+use routes::index::index;
+use routes::users::{get_users, create_role, create_user, update_user};
+
+#[catch(404)]
+fn not_found() -> Value {
+    json!({
+        "status": "error",
+        "reason": "Resource was not found."
+    })
+}
+
+#[catch(500)]
+fn server_error() -> Value {
+    json!({
+        "status": "Server error",
+        "reason": "Something went wrong. Please try again later"
+    })
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index, get_users, create_role, create_user, update_user])
+    .register("/", catchers![not_found, server_error])
 }
